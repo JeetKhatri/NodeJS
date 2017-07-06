be1a6c70f48992e70d73c1d1dcf6d356b4da3976
Jeet Khatri <jeet.g.khatri@gmail.com>
2017-07-07 01:28:55 +0530

    till 27 - Partial Templates kind of header

--- NodeJS_Prectice/server.js
+++ NodeJS_Prectice/server.js
@@ -5,12 +5,15 @@ app.set('view engine', 'ejs' ); // default engine specify
 
 
 // when user call http://127.0.0.1:3000 then the function execute.
-app.get('/',function(req,res){
+/*app.get('/',function(req,res){
   res.sendFile(__dirname+'/index.html');
+});*/
+app.get('/',function(req,res){
+  res.render('index');              // render method bydefault go to check in views folder
 });
 
-app.get('/error',function(req,res){
-  res.sendFile(__dirname+'/error.html');
+app.get('/contact',function(req,res){
+  res.render('contact');
 });
 
 app.get('/json',function(req,res){
@@ -18,7 +21,7 @@ app.get('/json',function(req,res){
   res.send(JSON.stringify(students));
 });
 
-app.get('/profile/:name',function(req,res){           // render method bydefault go to check in views folder
+app.get('/profile/:name',function(req,res){
   var data = {age: 21, clg: 'DAIICT',hobbies: ['cricket','gaming','tennis']};
   res.render('profile',{person: req.params.name,data: data});                              // send data in parameter
 });
