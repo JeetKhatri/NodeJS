eec91182dc2b366c9ed5c9e3993b3e9208778102
Jeet Khatri <jeet.g.khatri@gmail.com>
2017-07-07 02:16:57 +0530

    30 - Handling POST Requests

--- NodeJS_Prectice/server.js
+++ NodeJS_Prectice/server.js
@@ -1,5 +1,9 @@
 var express = require('express');    //because its a function
+var bodyParser = require('body-parser');   // https://www.npmjs.com/package/body-parser
+                                            // for post data
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set('view engine', 'ejs' ); // default engine specify
 
@@ -28,7 +32,13 @@ app.get('/profile/:name',function(req,res){
 });*/
   // Query String
 app.get('/profile',function(req,res){                                           //http://127.0.0.1:3000/profile?name=jeet&age=21
-  var data = {clg: 'DAIICT',hobbies: ['cricket','gaming','tennis']};
-  res.render('profile',{qs: req.query,data: data});                             // use query to url data convert into string
+  var data = {clg: 'DAIICT',hobbies: ['cricket','gaming','tennis']};            // work with get method
+  res.render('profile',{qs: req.query,data: data});                         // req.query :use query to url data convert into json
 });
+
+app.post('/contactAction',urlencodedParser,function(req,res){                   // use with post method
+  console.log(req.body);                                                    // req.body :post data convert into json
+  res.render('successMSG',{rb: req.body});
+});
+
 app.listen(3000);
