c4f2945a820aac0c2f199bccb2dbe4916796e502
Jeet Khatri <jeet.g.khatri@gmail.com>
2017-07-07 18:12:59 +0530

    ToDo app - 3

--- nodejs_app/controllers/todoController.js
+++ nodejs_app/controllers/todoController.js
@@ -1,13 +1,13 @@
-module.export = function(app){
+module.exports = function(app){
 
   app.get("/todo",function(req,res){
-
+      res.render('todo');
   });
 
   app.post("/todo",function(req,res){
 
   });
-  
+
   app.delete("/todo",function(req,res){
 
   });
